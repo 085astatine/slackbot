660f3393436f4b4facf64e5c8e6387a824caa272
Astatine <astatine085@gmail.com>
2016-08-07 03:56:40 +0900

    option parserを作成
    slack.SlackBot
        option parserを作成した.
        tokenが書かれたfile, loggerのlevelをoptionで指定出来るようにした.

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import pathlib
-from typing import Optional
 import slack
-
-def load_token() -> Optional[str]:
-    token_file = pathlib.Path('SLACK_TOKEN')
-    if token_file.exists():
-        with token_file.open() as file:
-            token = file.read().strip()
-        return token
-    else:
-        return None
 
 if __name__ == '__main__':
     # logger
@@ -22,9 +11,6 @@ if __name__ == '__main__':
     handler.formatter = logging.Formatter(
                 fmt= '%(name)s::%(levelname)s::%(message)s')
     logger.addHandler(handler)
-    token = load_token()
-    if token is not None:
-        slack_bot = slack.SlackBot(
-                    token,
-                    logger= logger)
-        slack_bot.run()
+    slack_bot = slack.SlackBot(
+                logger= logger)
+    slack_bot.run()
--- slack/_bot.py
+++ slack/_bot.py
@@ -1,20 +1,39 @@
 # -*- coding: utf-8 -*-
 
+import argparse
 import logging
+import pathlib
 import time
+from typing import Optional
 from slackclient import SlackClient
 
 class SlackBot:
     def __init__(
                 self,
-                token: str,
+                option: argparse.Namespace = None,
                 logger: logging.Logger = None) -> None:
+        # option
+        if option is None:
+            option = _create_option_parser().parse_args()
+        self._option = option
         # logger
         self._logger = (
                     logger
                     if logger is not None
                     else logging.getLogger(__name__))
-        self._token = token
+        if self._option.log_level is not None:
+            self._logger.setLevel(
+                            self._option.log_level)
+        self._logger.debug('option: {0}'.format(self._option))
+        # Token
+        self._token = _load_token(
+                    self._option.token_file,
+                    self._logger)
+        if self._token is None:
+            self._logger.error('load token: Failed')
+        else:
+            self._logger.debug('load token: Success')
+        # Client
         self._client = SlackClient(self._token)
     
     def run(self) -> None:
@@ -26,3 +45,52 @@ class SlackBot:
                 time.sleep(1)
         else:
             self._logger.error('Connects to the RTM WebSocket: Failed')
+
+def _create_option_parser() -> argparse.ArgumentParser:
+    root_parser = argparse.ArgumentParser(
+                description= 'SlackBot')
+    # SlackBot option
+    _slackbot_option_parser(root_parser)
+    return root_parser
+
+def _slackbot_option_parser(
+            root_parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+    parser = root_parser.add_argument_group(
+                title= 'SlackBot Options')
+    # Token File
+    parser.add_argument(
+                '--token-file',
+                dest= 'token_file',
+                type= pathlib.Path,
+                required= True,
+                help= ('set the path to the file '
+                       'that Slack Authentification token is written'))
+    # Log Level
+    parser.add_argument(
+                '--log-level',
+                dest= 'log_level',
+                action= _LogLevelAction,
+                choices= ('debug', 'info', 'warning', 'error', 'critical'),
+                help= 'set the threshold for the logger')
+    return parser
+
+class _LogLevelAction(argparse.Action):
+    def __call__(
+                self,
+                parser: argparse.ArgumentParser,
+                namespace: argparse.Namespace,
+                value: str,
+                option_string: str = None) -> None:
+        setattr(namespace, self.dest, getattr(logging, value.upper()))
+
+def _load_token(
+            token_file: pathlib.Path,
+            logger: logging.Logger) -> Optional[str]:
+    if not token_file.exists():
+        logger.error('Token File<{0}> does not exist'
+                    .format(token_file.as_posix()))
+        return None
+    else:
+        with token_file.open() as file:
+            token = file.read().strip()
+        return token
