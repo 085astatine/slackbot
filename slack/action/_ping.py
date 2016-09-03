37a171237914aba1e5fefb2537c5f9f72df1eaa7
Astatine <astatine085@gmail.com>
2016-09-03 21:20:48 +0900

    Ping pingを返す機能を追加
    slack.action.Ping
        pingの投稿にpongと返す機能を実装した.

--- slack/action/_ping.py
+++ slack/action/_ping.py
@@ -16,9 +16,15 @@ class Ping(SlackBotAction):
         log_level = getattr(option, '{0}.log_level'.format(self.name))
         if log_level is not None:
             self._logger.setLevel(log_level)
+        # Setting
+        self._target_text = 'ping'
+        self._reply_text = 'pong'
     
     def action(self, api_list: List[dict]) -> None:
-        pass
+        for api in api_list:
+            if _is_target(self, api):
+                self._logger.info('send Reply')
+                _send_reply(self, api)
     
     @staticmethod
     def option_parser(
@@ -35,3 +41,28 @@ class Ping(SlackBotAction):
                     choices= _LogLevelAction.choices(),
                     help= 'set the threshold for the logger')
         return root_parser
+
+def _is_target(
+            self: Ping,
+            api: dict) -> bool:
+    if api.get('type') == 'message':
+        if api.get('text') == self._target_text:
+            return True
+    return False
+
+def _send_reply(
+            self: Ping,
+            api: dict) -> None:
+    channel = self._team.channel_list.id_search(api.get('channel'))
+    if channel is None:
+        self._logger.error('unknown channel: {0}'.format(api.get('channel')))
+        return
+    reply_target = self._team.member_list.id_search(api.get('user'))
+    if reply_target is None:
+        self._logger.error('unknown target: {0}'.format(api.get('user')))
+        return
+    reply_text = '<@{0}> {1}'.format(reply_target.id, self._reply_text)
+    self._api_call(
+                method= 'chat.postMessage',
+                text= reply_text,
+                channel= channel.id)
