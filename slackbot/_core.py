5e6bbee558dcece80f071610103d8d51deb451b6
Astatine <astatine085@gmail.com>
2017-07-02 13:10:43 +0900

    create: command line による設定
    slackbot.Create
        command line optionによって設定を切り替えられるようにした.
            設定fileのpath指定
            設定file例を標準出力に出力
            log levelをdebugにする

--- slackbot/_core.py
+++ slackbot/_core.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 
+import argparse as _argparse
+import collections as _collections
 import logging as _logging
+import pathlib as _pathlib
+import sys as _sys
+import yaml as _yaml
 from ._action import Action
 from ._config import ConfigParser, Option
 
@@ -28,19 +33,71 @@ class Core(Action):
         return tuple()
 
 
-def create(name, action_list=None, logger=None):
+def create(
+            name,
+            action_list=None,
+            logger=None,
+            argv=None):
     # logger
     if logger is None:
         logger = _logging.getLogger(__name__)
     # action list
     if action_list is None:
         action_list = dict()
+    # argument parser
+    argument_parser = _argparse.ArgumentParser(
+                description='SlackBot: {0}'.format(name))
+    argument_parser.add_argument(
+                '--config',
+                dest='config',
+                type=_pathlib.Path,
+                metavar='YAML_FILE',
+                help='set the configuration file in yaml')
+    argument_parser.add_argument(
+                '--show-config',
+                dest='show_config',
+                action='store_true',
+                help='output example of configuration file '
+                     'to standard output')
+    argument_parser.add_argument(
+                '-v', '--verbose',
+                dest='verbose',
+                action='store_true',
+                help='set log level to debug')
+    option = argument_parser.parse_args(argv)
+    # log level
+    if option.verbose:
+        logger.setLevel(_logging.DEBUG)
+    logger.debug('command line option: {0}'.format(option))
     # config parser
-    config_parser_list = []
-    config_parser_list.append(ConfigParser('Core', Core.option_list()))
-    config_parser_list.extend(
-                ConfigParser(key, action_list[key].option_list())
+    config_parser_list = _collections.OrderedDict()
+    config_parser_list['Core'] = ConfigParser('Core', Core.option_list())
+    config_parser_list.update(
+                (key, ConfigParser(key, action_list[key].option_list()))
                 for key in sorted(action_list.keys()))
+    # show example of configuration file
+    if option.show_config:
+        logger.info('output example of configuration file')
+        _sys.stdout.write('{0}\n'.format(
+                    '\n'.join(parser.help_message()
+                              for parser in config_parser_list.values())))
+        _sys.exit(0)
+    # load configuration file
+    if option.config is None:
+        message = 'configuration file is not selected'
+        logger.error(message)
+        _sys.stderr.write('{0}\n'.format(message))
+        _sys.exit(1)
+    elif not option.config.exists():
+        message = 'configuration file({0}) does not exit'.format(
+                    option.config.as_posix())
+        logger.error(message)
+        _sys.stderr.write('{0}\n'.format(message))
+        _sys.exit(1)
+    config_yaml = _yaml.load(option.config.open())
+    config_list = {key: parser.parse(config_yaml.get(key, None))
+                   for key, parser in config_parser_list.items()}
+    logger.debug('config: {0}'.format(config_list))
     return Core(name,
                 action_list={
                     key: action(key)
