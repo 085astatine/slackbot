51370a9b123dac95279d468e353ff3673ed0da82
Astatine <astatine085@gmail.com>
2017-07-05 04:16:50 +0900

    Action: APIを呼び出す関数を作成
    slackbot.Action
        APIを呼び出す関数を追加した.

--- slackbot/_action.py
+++ slackbot/_action.py
@@ -24,6 +24,16 @@ class Action(object):
     def run(self, api_list):
         pass
 
+    def api_call(self, method, **kwargs):
+        self._logger.debug("call API '{0}': {1}".format(method, kwargs))
+        result = self._client.api_call(method, **kwargs)
+        self._logger.log(
+                    (_logging.DEBUG
+                        if result.get('ok', False)
+                        else _logging.ERROR),
+                    'result: {0}'.format(result))
+        return result
+
     @property
     def name(self):
         return self._name
