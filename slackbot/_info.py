82e56a19f0090ab2369d954d4c3ea464caab53ef
Astatine <astatine085@gmail.com>
2017-07-06 18:02:08 +0900

    Team: team.infoの情報を扱うclassを作成
    slackbot._info.Team
        team.infoで得られる情報を扱うclass.
        APIが返したdictをinstance変数として持つ.

--- slackbot/_info.py
+++ slackbot/_info.py
@@ -5,6 +5,26 @@ import logging as _logging
 from ._action import Action
 
 
+class Team(object):
+    def __init__(self, data):
+        self._data = data
+
+    def get(self, key):
+        return self._data[key]
+
+    def update(self, data):
+        self._data.clear()
+        self._data.update(data)
+
+    @property
+    def id(self):
+        return self._data['id']
+
+    @property
+    def name(self):
+        return self._data['name']
+
+
 class InfoUpdate(Action):
     def __init__(
                 self,
@@ -18,3 +38,10 @@ class InfoUpdate(Action):
                     (logger
                         if logger is not None
                         else _logging.getLogger(__name__)))
+
+    def setup(self, client):
+        Action.setup(self, client)
+        # team
+        team = Team(self.api_call('team.info')['team'])
+        self._logger.debug("team id: '{0}'".format(team.id))
+        self._logger.debug("team name: '{0}'".format(team.name))
