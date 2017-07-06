63b355a2856d11f3de64a7cc2ff5bdce73a223e4
Astatine <astatine085@gmail.com>
2017-07-06 19:40:31 +0900

    User: user objectを扱うclassを作成
    slackbot._user.User
        user objectの情報を扱うclass
        APIが返したobjectをinstance変数として持つ.

--- slackbot/_info.py
+++ slackbot/_info.py
@@ -25,6 +25,26 @@ class Team(object):
         return self._data['name']
 
 
+class User(object):
+    def __init__(self, data):
+        self._data = data
+
+    def get(self, key):
+        return self._data[key]
+
+    def update(self, data):
+        self._data.clear()
+        self._data.update(data)
+
+    @property
+    def id(self):
+        return self._data['id']
+
+    @property
+    def name(self):
+        return self._data['name']
+
+
 class InfoUpdate(Action):
     def __init__(
                 self,
@@ -45,3 +65,9 @@ class InfoUpdate(Action):
         team = Team(self.api_call('team.info')['team'])
         self._logger.debug("team id: '{0}'".format(team.id))
         self._logger.debug("team name: '{0}'".format(team.name))
+        # user
+        users_info = self.api_call('users.list')
+        for user_type in users_info['members']:
+            user = User(user_type)
+            self._logger.debug("user id: '{0}'".format(user.id))
+            self._logger.debug("user name: '{0}'".format(user.name))
