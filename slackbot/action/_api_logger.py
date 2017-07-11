be43316752fb353a05a97a9c5d1711fe063820ca
Astatine <astatine085@gmail.com>
2017-07-11 17:16:10 +0900

    APILogger: presence_change, user_typingのon/off切替
    slackbot.action.APILogger
        presence_changeとuser_typingを出力するか否かを設定するOptionを追加した.

--- slackbot/action/_api_logger.py
+++ slackbot/action/_api_logger.py
@@ -2,7 +2,7 @@
 
 
 import logging as _logging
-from .. import Action
+from .. import Action, Option
 
 
 class APILogger(Action):
@@ -21,4 +21,23 @@ class APILogger(Action):
 
     def run(self, api_list):
         for api in api_list:
+            # ignore check
+            if (not self.config.output_presence_change and
+                    api['type'] == 'presence_change'):
+                continue
+            if (not self.config.output_user_typing and
+                    api['type'] == 'user_typing'):
+                continue
             self._logger.info(api)
+
+    @staticmethod
+    def option_list():
+        return (
+            Option('output_presence_change',
+                   type=bool,
+                   default=True,
+                   help='output presence_change'),
+            Option('output_user_typing',
+                   type=bool,
+                   default=True,
+                   help='output user_typing'))
