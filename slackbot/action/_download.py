926ec08d68206e5c5dc343e54ed5de70c5674f8b
Astatine <astatine085@gmail.com>
2017-08-21 14:13:15 +0900

    Download命令の検知
    slackbot.action.Download
        対象channelを指定するoptionを作成した.
        命令の正規表現を指定するoptionを作成した.
        対象channelに命令が投稿されたことを検知できるようになった.

--- slackbot/action/_download.py
+++ slackbot/action/_download.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8
 
 
+import re
 import logging
 from typing import Any, Dict, List, Optional, Tuple
 from .. import Action, Option
@@ -20,8 +21,29 @@ class Download(Action):
                         else logging.getLogger(__name__)))
 
     def run(self, api_list: List[Dict[str, Any]]) -> None:
-        pass
+        for api in api_list:
+            if api['type'] == 'message' and 'subtype' not in api:
+                channel = self.info.channel_list.id_search(api['channel'])
+                if channel is None or channel.name not in self.config.channel:
+                    continue
+                match = self.config.pattern.match(api['text'].strip())
+                if match:
+                    name = match.group('name')
+                    url = match.group('url')
+                    self._logger.info('name: {0}'.format(name))
+                    self._logger.info('url: {0}'.format(url))
 
     @staticmethod
     def option_list() -> Tuple[Option, ...]:
-        return tuple()
+        return (
+            Option('channel',
+                   action=lambda x: [x] if isinstance(x, str) else x,
+                   default=[],
+                   help='target channel name (list or string)'),
+            Option('pattern',
+                   action=re.compile,
+                   default=r'download\s+"(?P<name>.+)"\s+'
+                           r'<(?P<url>https?://[\w/:%#\$&\?\(\)~\.=\+\-]+)'
+                           r'(|\|[^>]+)>',
+                   help=('regular expresion for working'
+                         'which have simbolic groups named "name" & "url"')))
