e40dbae045067a6b71665f00a3b07858c05e1c00
Astatine <astatine085@gmail.com>
2018-09-23 23:36:18 +0900

    pattern optionを作成

--- slackbot/action/_response.py
+++ slackbot/action/_response.py
@@ -4,8 +4,8 @@ import enum
 import logging
 import re
 from collections import OrderedDict
-from typing import Any, Dict, List, Optional, Tuple
-from .. import Action, Option, unescape_text
+from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
+from .. import Action, Option, OptionError, unescape_text
 from .._team import Channel, User
 
 
@@ -13,6 +13,30 @@ class Trigger(enum.Enum):
     NON_REPLY = enum.auto()
     REPLY = enum.auto()
     ANY = enum.auto()
+
+
+class Pattern:
+    def __init__(
+            self,
+            call: Union[str, Iterable[str]],
+            response: Union[str, Iterable[str]]) -> None:
+        self.call: Tuple[str, ...] = (
+                tuple(call)
+                if not isinstance(call, str)
+                else (call,))
+        self.response: Tuple[str, ...] = (
+                tuple(response)
+                if not isinstance(response, str)
+                else (response,))
+        assert(all(map(lambda x: isinstance(x, str), self.call)))
+        assert(all(map(lambda x: isinstance(x, str), self.response)))
+
+    def __repr__(self) -> str:
+        return "{0}.{1}(call={2}, response={3})".format(
+                self.__class__.__module__,
+                self.__class__.__name__,
+                repr(self.call),
+                repr(self.response))
 
 
 class Response(Action):
@@ -45,11 +69,51 @@ class Response(Action):
 
     @staticmethod
     def option_list() -> Tuple[Option, ...]:
-        # translate: str -> Trigger
+        # translate to Trigger
         to_trigger: Dict[str, Trigger] = OrderedDict()
         to_trigger['non-reply'] = Trigger.NON_REPLY
         to_trigger['reply'] = Trigger.REPLY
         to_trigger['any'] = Trigger.ANY
+
+        # translate to Pattern
+        def parse_pattern(data: Any) -> Pattern:
+            if isinstance(data, Pattern):
+                return data
+            # type check
+            elif (isinstance(data, dict)
+                    and 'call' in data
+                    and (isinstance(data['call'], str)
+                         or (hasattr(data['call'], '__iter__')
+                             and all(map(lambda x: isinstance(x, str),
+                                         data['call']))))
+                    and 'response' in data
+                    and (isinstance(data['response'], str)
+                         or (hasattr(data['response'], '__iter__')
+                             and all(map(lambda x: isinstance(x, str),
+                                         data['response']))))
+                    and len(data) == 2):
+                return Pattern(**data)
+            else:
+                message = (
+                        'could not convert to Pattern: \'{0}\''
+                        .format(data))
+                raise OptionError(message)
+
+        # translate to list of Patterns
+        def parse_pattern_list(data: Any) -> List[Pattern]:
+            result: List[Pattern] = []
+            if isinstance(data, dict):
+                result.append(parse_pattern(data))
+            elif hasattr(data, '__iter__') and not isinstance(data, str):
+                for element in data:
+                    result.append(parse_pattern(element))
+            else:
+                message = (
+                    'could not convert to Pattern\'s list: \'{0}\''
+                    .format(data))
+                raise OptionError(message)
+            return result
+
         return (
             Option('channel',
                    action=lambda x: [x] if isinstance(x, str) else x,
@@ -60,6 +124,11 @@ class Response(Action):
                    action=lambda x: to_trigger.get(x),
                    choices=to_trigger.keys(),
                    help='response trigger'),
+            Option('pattern',
+                   default=(Pattern(call='ping', response='pong'),),
+                   action=parse_pattern_list,
+                   help='response pattern'
+                        ' (default: [{call: ping, response: pong}])'),
             Option('word',
                    type=str,
                    action=str.strip,
