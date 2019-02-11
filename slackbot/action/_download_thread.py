b6081332be4c3d0919d5edf1b70f45ea9dc0a2ca
Astatine <astatine085@gmail.com>
2019-02-11 13:45:37 +0900

    instance変数の初期化を修正

--- slackbot/action/_download_thread.py
+++ slackbot/action/_download_thread.py
@@ -228,6 +228,7 @@ class Reporter(Generic[ReportInfo]):
         self._info = info
         self._url = url
         self._path = path
+        self._final_url: Optional[str] = None
         self._temp_path: Optional[pathlib.Path] = None
         self._response_header: Optional[MutableMapping[str, str]] = None
         self._saved_path: Optional[pathlib.Path] = None
