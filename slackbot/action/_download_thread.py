8b940011983b9f5aa17cf57daf42bdc5a740f703
Astatine <astatine085@gmail.com>
2019-02-12 12:16:53 +0900

    'super().__init__()'に変更

--- slackbot/action/_download_thread.py
+++ slackbot/action/_download_thread.py
@@ -301,7 +301,7 @@ class DownloadThread(threading.Thread, Generic[ReportInfo]):
                 path: pathlib.Path,
                 url: str,
                 option: DownloadThreadOption) -> None:
-        threading.Thread.__init__(self)
+        super().__init__()
         self._info = info
         self._report_queue = report_queue
         self._path = path
