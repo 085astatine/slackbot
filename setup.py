ab8dfc4f15ef6ca456fbfea30833fc4bdd0bdd51
Astatine <astatine085@gmail.com>
2019-03-07 12:50:33 +0900

    setup.cfgを作成

--- setup.py
+++ setup.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from setuptools import setup
 
-setup(
-    name='slackbot',
-    version='0.0.0',
-    author='Astatine',
-    author_email='astatine085@gmail.com',
-    url='https://github.com/085astatine/slackbot',
-    packages=[
-        'slackbot',
-        'slackbot.action'],
-    install_requires=[
-        'pyyaml>=4.2b1',
-        'requests>=2.20',
-        'slackclient'],
-    test_suite='test')
+
+setup()
